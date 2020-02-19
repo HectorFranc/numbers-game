d8d2695681cfd8bab6dc7bf72e6a5e1e871786e1
Hector Franco Torres <tor6.franco6@hotmail.com>
2020-02-18 21:22:53 -0600

    Init game in main element

--- js/index.js
+++ js/index.js
@@ -1,5 +1,3 @@
 import Game from './Game.js'
 
-let game = new Game(2, ['Hector', 'Santi'], document.querySelector('main'))
-
-console.log(game)
+let game = new Game(2, ['Player 1', 'Player 2'], document.querySelector('main'))
